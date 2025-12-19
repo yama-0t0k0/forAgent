28f934400217936dc4d40b9b6b7bb5d888862ba5
Lycaon <tbc.yamakawa@gmail.com>
2025-12-20 01:21:40 +0900

    Fix App.js navigation to default to MyPageScreen after sync revert

--- App.js
+++ App.js
@@ -7,19 +7,35 @@ import { DataProvider } from './src/context/DataContext';
 import { GenericRegistrationScreen } from './src/screens/GenericRegistrationScreen';
 import { THEME } from './src/constants/theme';
 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MyPageScreen } from './src/screens/MyPageScreen';
+
 const ENGINEER_TEMPLATE = require('./assets/json/engineer-profile-template.json');
 const COMPANY_TEMPLATE = require('./assets/json/company-profile-template.json');
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const EngineerNavigator = () => (
+  <Stack.Navigator initialRouteName="MyPage" screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Registration">
+      {(props) => (
+        <GenericRegistrationScreen
+          {...props}
+          title="エンジニア個人登録"
+          collectionName="individual"
+          idField="id_individual"
+          idPrefixChar="C"
+        />
+      )}
+    </Stack.Screen>
+    <Stack.Screen name="MyPage" component={MyPageScreen} />
+  </Stack.Navigator>
+);
 
 const EngineerRegistrationWrapper = () => (
   <DataProvider initialData={ENGINEER_TEMPLATE}>
-    <GenericRegistrationScreen
-      title="エンジニア個人登録"
-      collectionName="individual"
-      idField="id_individual"
-      idPrefixChar="C"
-    />
+    <EngineerNavigator />
   </DataProvider>
 );
 
