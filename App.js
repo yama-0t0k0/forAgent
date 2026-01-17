5958dc0729cdfd4ae18379f2b11719bfa8758814
teruroom <teruroom@gmail.com>
2026-01-17 14:55:30 +0900

    feat: implement tab navigation and host separate registration apps on firebase

--- App.js
+++ App.js
@@ -7,32 +7,11 @@ import { DataProvider } from './src/context/DataContext';
 import { GenericRegistrationScreen } from './src/screens/GenericRegistrationScreen';
 import { THEME } from './src/constants/theme';
 
-const ENGINEER_TEMPLATE = require('./assets/json/engineer-profile-template.json');
-const COMPANY_TEMPLATE = require('./assets/json/company-profile-template.json');
 
 const Tab = createBottomTabNavigator();
 
-const EngineerRegistrationWrapper = () => (
-  <DataProvider initialData={ENGINEER_TEMPLATE}>
-    <GenericRegistrationScreen
-      title="エンジニア個人登録"
-      collectionName="individual"
-      idField="id_individual"
-      idPrefixChar="C"
-    />
-  </DataProvider>
-);
-
-const CompanyRegistrationWrapper = () => (
-  <DataProvider initialData={COMPANY_TEMPLATE}>
-    <GenericRegistrationScreen
-      title="企業プロフィール登録"
-      collectionName="company"
-      idField="id"
-      idPrefixChar="B"
-    />
-  </DataProvider>
-);
+import { EngineerRegistrationWrapper } from './src/apps/EngineerApp';
+import { CompanyRegistrationWrapper } from './src/apps/CompanyApp';
 
 export default function App() {
   const appMode = process.env.EXPO_PUBLIC_APP_MODE; // 'engineer' | 'company'
